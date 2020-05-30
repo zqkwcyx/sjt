226e1fc02fd5f38a7c001c041ab63b856889e766
kuangweicong <870242856@qq.com>
2020-05-30 10:57:11 +0800

    first commit

---
+++ src/main/java/cn/cong/MyApplication.java
@@ -0,0 +1,11 @@
+package cn.cong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
